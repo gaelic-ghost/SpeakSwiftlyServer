7efe6579867630de0ce2f2932ad867c1f25c1446
Gale W <mail@galewilliams.com>
2026-04-16 12:09:33 -0400

    deps: update speakswiftly to 3.0.5

--- Package.swift
+++ Package.swift
@@ -31,10 +31,10 @@ let package = Package(
         .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
         .package(
             url: "https://github.com/gaelic-ghost/SpeakSwiftly.git",
-            from: "3.0.4",
+            from: "3.0.5",
         ),
         .package(url: "https://github.com/ml-explore/mlx-swift-lm.git", exact: "2.30.6"),
-        .package(url: "https://github.com/gaelic-ghost/TextForSpeech.git", from: "0.16.0"),
+        .package(url: "https://github.com/gaelic-ghost/TextForSpeech.git", from: "0.17.0"),
         .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.1.3"),
         .package(url: "https://github.com/modelcontextprotocol/swift-sdk.git", from: "0.12.0"),
         .package(
